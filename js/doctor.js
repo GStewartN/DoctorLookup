ce638f21cf2fba94ebd4306daf684765a3ecfbac
Nathan Stewart <stewart.nathang@gmail.com>
2017-08-04 18:34:47 -0700

    add sass styling, increase results param

--- js/doctor.js
+++ js/doctor.js
@@ -4,7 +4,7 @@ Doctor = function() {
 };
 
 Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=or-portland&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=' + apiKey)
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=or-portland&user_location=45.5231%2C-122.6765&skip=0&limit=15&user_key=' + apiKey)
    .then(function(response) {
       displayDoctors(response.data);
     })
@@ -12,6 +12,5 @@ Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors) {
       console.log("fail");
     });
 };
-
 
 exports.doctorModule = Doctor;
